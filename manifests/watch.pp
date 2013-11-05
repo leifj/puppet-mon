95f58e6cc2ba0edb9b3b84d41957a607a75670af
Leif Johansson <leifj@sunet.se>
2013-11-05 01:48:05 +0100

    use name

--- manifests/watch.pp
+++ manifests/watch.pp
@@ -1,5 +1,5 @@
-define mon::watch ($hostgroup,$mail,$service='ping',$interval='5m',$monitor='fping.monitor') {
-  concat::fragment {"mon_watch_${hostgroup}_${service}":
+define mon::watch ($mail,$service='ping',$interval='5m',$monitor='fping.monitor') {
+  concat::fragment {"mon_watch_${name}_${service}":
     target  => '/etc/mon/mon.cf',
     content => templates('mon/watch.erb'),
     order   => 30,
