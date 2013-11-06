e7baf8b456690ba92a9198a0613b37bfc450176e
Leif Johansson <leifj@sunet.se>
2013-11-06 02:56:55 +0100

    python pip

--- manifests/debian.pp
+++ manifests/debian.pp
@@ -1,5 +1,6 @@
 class mon::debian inherits mon::base {
   package {'mon': ensure => installed }
+  package {'python-pip': ensure => installed }
   include python
   python::pip {'python-pushover': ensure => present}
   Service['mon'] {
