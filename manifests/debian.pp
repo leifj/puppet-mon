e1186affd8d787e2c764f8329f8bf2499a203917
Leif Johansson <leifj@sunet.se>
2013-11-06 02:48:09 +0100

    pushover

--- manifests/debian.pp
+++ manifests/debian.pp
@@ -1,7 +1,12 @@
 class mon::debian inherits mon::base {
   package {'mon': ensure => installed }
+  package {'python-pushover': ensure => installed}
   Service['mon'] {
     pattern => '/usr/sbin/mon',
     restart => 'service mon restart',
+  }
+  file {'/usr/lib/mon/alert.d/pushover.alert':
+     ensure  => present,
+     source  => 'puppet:///modules/mon/pushover.alert'
   }
 }
