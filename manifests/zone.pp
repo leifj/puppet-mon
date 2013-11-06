5e1100dcdfa9abc01ce42de49b324439a18bf9cb
Leif Johansson <leifj@sunet.se>
2013-11-06 05:34:18 +0100

    define hostgroup for zone

--- manifests/zone.pp
+++ manifests/zone.pp
@@ -1,4 +1,5 @@
 define mon::zone($master,$slaves,$mail) {
+   mon::hostgroup{$name: members => $master}
    mon::watch{$name:
       service => $name,
       monitor => "dns.monitor -zone $name -master $master $slaves",
