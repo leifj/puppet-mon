61f6fc905f57a756dcd7e4247cf3d23107681dac
Leif Johansson <leifj@sunet.se>
2013-11-07 22:48:43 +0100

    dnsquery

---
+++ manifests/dnsquery.pp
@@ -0,0 +1,8 @@
+define mon::zone($name,$server,$mail) {
+   mon::hostgroup{$name: members => $server}
+   mon::watch{$name:
+      service => "dnsquery",
+      monitor => "dns-query.monitor -n $name $server",
+      mail    => $mail 
+   }
+}
