489bd721855d553d37016b5cd68b6d3ed5501656
Leif Johansson <leifj@sunet.se>
2013-11-07 04:04:31 +0100

    uri monitor class

---
+++ manifests/uri.pp
@@ -0,0 +1,4 @@
+define mon::uri($uri,$pattern,$mail) {
+  mon::hostgroup{$name: members => 'localhost'}
+  mon::watch{$name: service => "uri", monitor => "uri.monitor $uri $pattern", mail => $mail}
+}
