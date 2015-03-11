54a5e1d96163bc54a6b6a678b5d3c75cadb27a63
Leif Johansson <leifj@sunet.se>
2015-03-12 00:58:21 +0100

    not neded

--- manifests/vuri.pp
+++
@@ -1,4 +0,0 @@
-define mon::vuri($uri,$pattern,$hosts,$mail) {
-  mon::hostgroup{$name: members => $hosts}
-  mon::watch{$name: service => "uri", monitor => "vuri.monitor $uri \"$pattern\" $hosts", mail => $mail}
-}
