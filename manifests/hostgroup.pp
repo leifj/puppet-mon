c38a372188c443d62a47394fbecce9f502c10b83
Leif Johansson <leifj@sunet.se>
2015-08-15 02:31:32 +0200

    fix future parser issues

--- manifests/hostgroup.pp
+++ manifests/hostgroup.pp
@@ -2,7 +2,7 @@ define mon::hostgroup ($members=[]) {
   concat::fragment {"mon_hostgroup_${name}":
     target  => '/etc/mon/mon.cf',
     content => template('mon/hostgroup.erb'),
-    order   => 30,
+    order   => '30',
     notify  => Service['mon']
   }
 }
